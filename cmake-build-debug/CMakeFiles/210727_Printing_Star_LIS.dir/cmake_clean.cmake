dcab584da27783ddbfd146d7325bd4a31c0115c7
이인성 <isaac@iinseongs-MacBook-Pro.local>
2021-07-24 12:38:05 +0900

    solution

---
+++ cmake-build-debug/CMakeFiles/210727_Printing_Star_LIS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "210727_Printing_Star_LIS"
+  "210727_Printing_Star_LIS.pdb"
+  "CMakeFiles/210727_Printing_Star_LIS.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/210727_Printing_Star_LIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
